887e085f6714fa0ea39547b452ab046322752b2e
Nicole Ju <nicoleju@g.ucla.edu>
2023-11-27 21:37:39 -0800

    milestone 4 work (not complete)

--- Features/Feed/Screens/Feed.jsx
+++ Features/Feed/Screens/Feed.jsx
@@ -4,6 +4,7 @@ import Post from '../Components/Post';
 import InteractivePost from '../Components/InteractivePost';
 import React, { useState, useEffect } from 'react';
 import { useRoute } from '@react-navigation/native'; 
+import { Dropdown } from 'react-native-element-dropdown';
 
 
 export default function Feed({ navigation }) {
@@ -44,6 +45,33 @@ export default function Feed({ navigation }) {
     setId(id+1);
   }
 
+  const [sortOption, setSortOption] = useState('username ascending');
+  useEffect(() => {
+    //sort the postArray
+    newArray = [...postArray];
+    newArray.sort({sortFunction})
+    setPostArray(newArray);
+  }, [sortOption]);
+
+  const sortFunction = (a,b) => {
+    switch(sortOption) {
+      case 'username ascending':
+        console.log('1')
+        return a.username.localCompare(b.username)
+      case 'username descending':
+        console.log('2')
+        return b.username.localCompare(a.username)
+      case 'timestamp ascending':
+        console.log('3')
+        return a.time.localCompare(b.time)
+      case 'timestamp descending':
+        console.log('4')
+        return b.time.localCompare(a.time)
+      default:
+        return a.username.localCompare(b.username)
+
+    }
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -55,6 +83,13 @@ export default function Feed({ navigation }) {
       {GIVEN_POSTS.map((x) => (
         <Post key={x._id} username={x.username} body={x.body} />
       ))} */}
+      <Dropdown
+        data={data}
+        value={sortOption}
+        onChange={item => {
+          setSortOption(item.value);
+        }}
+      />
       <InteractivePost updateParentArray={updatePostArray}/>
       {postArray.map((x) => (
         <Post key={x._id} username={x.username} body={x.body} navigateToDetails={navigateToDetails} time={x.time}/>
